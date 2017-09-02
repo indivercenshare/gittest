a349558ca94ca98a1586a6150bd3ce196564994f
Indiver Kumar <ink@censhare.com>
2017-09-02 23:33:11 +0530

    MASTER

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,7 @@ public static void main(String[] args) {
 	System.out.println("test");
 	System.out.println("test1");
 	System.out.println("test2");
+	System.out.println("test4");
+	System.out.println("test5");
 }
 }
